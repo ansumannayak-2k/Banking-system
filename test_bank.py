97b9b8a89ec2c10c2c1bdae92db28c76d678c53a
Md-Anees-Alam <mohammadanees03@gmail.com>
2025-03-29 06:03:50 +0530

    banking test cases without edge cases

---
+++ test_bank.py
@@ -0,0 +1,49 @@
+import pytest
+from bank import BankAccount
+
+
+def test_deposit():
+    # Arrange
+    account = BankAccount(balance=100)
+
+
+    # Act
+    account.deposit(50)
+
+
+    # Assert
+    assert account.balance == 150
+
+
+def test_withdraw_sufficient_balance():
+    # Arrange
+    account = BankAccount(balance=100)
+
+
+    # Act
+    account.withdraw(40)
+
+
+    # Assert
+    assert account.balance == 60
+
+
+def test_withdraw_insufficient_balance():
+    # Arrange
+    account = BankAccount(balance=100)
+
+
+    # Act & Assert
+    with pytest.raises(ValueError, match="Insufficient funds!"):
+        account.withdraw(200)
+
+
+def test_deposit_negative_amount():
+    # Arrange
+    account = BankAccount(balance=100)
+
+
+    # Act & Assert
+    with pytest.raises(ValueError, match="Deposit amount must be positive"):
+        account.deposit(-50)
+
